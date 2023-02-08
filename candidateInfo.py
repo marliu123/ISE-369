90232bdfc629d7ee136bd062a93cb0dac5c1fb81
Marcus Liu <marcus.liu@stonybrook.edu>
2023-02-08 11:57:38 -0500

    edit

---
+++ candidateInfo.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+os.chdir('/Users/marcusliu/Documents/ISE-369')
+df = pd.read_csv('weball22.txt', sep='|', header=None)
+df=df.loc[:,[0,1,2,4,5,18,23]]
+df = df.rename(columns = {0:"ID"})
+df = df.rename(columns = {1:"Name"})
+df = df.rename(columns = {2:"I/C"})
+df = df.rename(columns = {4:"Pty"})
+df = df.rename(columns = {5:"$$"})
+df = df.rename(columns = {18:"State"})
+df = df.rename(columns = {23:"Status"})
+print(df)
